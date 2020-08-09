a762d07e1945c38b715d81b7bf3fdfef0d86bf71
Akiko <65807177+akiko-hi@users.noreply.github.com>
2020-08-09 15:36:07 +1200

    Add nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import logo from './images/logo.png';
 
 function App() {
   return (
     <div className="App">
-      
+      <nav>
+      <div className="company-logo">
+      <div className="name">COUNTUP</div>
+      <img className="logo" src={logo} alt="logo" />
+      </div>
+        <ul>
+          <li>Home</li>
+          <li>Cart</li>
+          <li>Favorites</li>
+          <li>Settings</li>
+        </ul>
+      </nav>
+
     </div>
   );
 }
