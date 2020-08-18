333a5f43f824e33f96a2c99176cccf37f39e8807
Akiko <65807177+akiko-hi@users.noreply.github.com>
2020-08-18 19:09:09 +1200

    Enable Products to scroll down

--- src/Seach.tsx
+++ src/Seach.tsx
@@ -11,25 +11,21 @@ export default function Search() {
     const dispatch = useDispatch()
     const [searchWord, setSearhWord] = useState("")
 
-    async function searchProduct(word: string) {
-        if (word === "") {
+    async function onSubmit(e: React.FormEvent) {
+        e.preventDefault()
+        if (searchWord === "") {
             dispatch(actions.setSearchResult([]))
         } else {
-            const res = await searchProducts(word)
+            const res = await searchProducts(searchWord)
             dispatch(actions.setSearchResult(res))
         }
-    }
-
-    function onSubmit(e: React.FormEvent) {
-        e.preventDefault()
-        searchProduct(searchWord)
     }
 
     return <form className="Search" onSubmit={onSubmit}>
 
         <div className="input-container">
             <input onChange={e => setSearhWord(e.currentTarget.value)} value={searchWord} autoFocus />
-            <button onClick={() => searchProduct(searchWord)} className="search-btn">
+            <button className="search-btn">
                 <img className="searchImg" src={search} alt="search button" />
             </button>
         </div>
