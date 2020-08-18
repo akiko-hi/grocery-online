a30aae0363487cfc24e177f4b83287a347e021fc
Akiko <65807177+akiko-hi@users.noreply.github.com>
2020-08-18 18:17:36 +1200

    Animate CategoryCard

--- src/CategoryCard.tsx
+++ src/CategoryCard.tsx
@@ -2,17 +2,18 @@ import './CategoryCard.scss';
 import React from 'react';
 import { Category } from './api';
 import { useDispatch } from 'react-redux';
-import { actions } from './store';
+import { actions, useSelector } from './store';
 
 type CategoryCardProps = {
     category: Category
 }
 
 export default function CategoryCard({ category }: CategoryCardProps) {
-  const dispatch = useDispatch()
 
+  const dispatch = useDispatch()
+  const selectedCategory = useSelector(s => s.categoryId)
    
-  return <div className="CategoryCard" onClick={() => dispatch(actions.pickCategory(category.id))}>
+  return <div className={selectedCategory === category.id ? "CategoryCard active" : "CategoryCard"} onClick={() => dispatch(actions.pickCategory(category.id))}>
         <p style={{background: category.color}}>{category.name}</p>
         <img src={"/images/" + category.image} alt="product"/>
     </div>
